d3a4a7fa192d7e6458b193bfdee2e7d644e3887c
mk-espinoza <marina.kim.espinoza@gmail.com>
2023-07-16 12:51:20 -0400

    Created transition animations and added them to both scenes

---
+++ Chem-Cards-App/Assets/DataFiles/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transitionAnimator;
+    public float transitionTime = 1f;
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadLevel("ARScene"));
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        // Play animation
+        transitionAnimator.SetTrigger("StartTransitionTrigger");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load scene
+        SceneManager.LoadScene(sceneName);
+    }
+}
